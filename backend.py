837a159cecf925330b917781f5b8982ffb7866b2
Caio Assis <caioassis@Users-MacBook-Pro.local>
2024-06-13 23:52:27 -0300

    refact(inserir_dados):adicionando retorno com a lista de intes que foram adicionados no dataBase.

--- backend.py
+++ backend.py
@@ -5,7 +5,8 @@ cursor = conn.cursor()
 
 
 def inserir_dados_backend(listaDados):
-    
+
+    listaDadosInseridos = []
     try:
         consulta = """
             INSERT INTO clientes (nome, sobrenome, email, cpf)
@@ -15,9 +16,24 @@ def inserir_dados_backend(listaDados):
         cursor.execute(consulta, listaDados)
 
         conn.commit()
-        return True
+        return True,listaDados
     
     except Exception as e:
+        print(e)
         return str(e)
 
-
+def buscar_todos_dados_backend():
+    
+    listaCadastros = []
+    try:
+        
+        cursor.execute("SELECT * FROM clientes")
+        
+        for row in cursor.fetchall():
+            listaCadastros.append(row)
+        
+        return True,listaCadastros
+        
+    except Exception as e:
+        return str(e)
+    

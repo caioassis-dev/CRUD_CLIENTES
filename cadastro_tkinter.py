bdc437960d66359546a81f15378f929a3f61bac8
Caio Assis <caioassis@Users-MacBook-Pro.local>
2024-06-13 23:54:30 -0300

    refact(text_widget):adicionando delete para linha do text_widget sempre quando for enviar nova mensagem, nova requisicao

--- cadastro_tkinter.py
+++ cadastro_tkinter.py
@@ -2,15 +2,23 @@ from tkinter import *
 import tkinter as tk
 import backend
 from tkinter import messagebox
+import pdb
 
-def obter_dados_input():
+
+def buscar_pelo_cpf():
     pass
 
 def buscar_todos_dados():
-    pass
+    
+    resposta,row = backend.buscar_todos_dados_backend()
+    # adicionei essa linha abaixo para limpar a saida toda vez que for uma noa requisição
+    text_widget.delete("1.0","end") 
+    for cadastro in row:
 
-def buscar():
-    pass
+        text_widget.insert("end",f"{cadastro}\n")
+    
+    return resposta
+
 
 def inserir_dados():
     listaValores = []
@@ -23,12 +31,16 @@ def inserir_dados():
         listaValores.append(email.get())
     if cpf.get():
         listaValores.append(cpf.get())
-
-    resposta = backend.inserir_dados_backend(listaValores)
     
+    resposta, row = backend.inserir_dados_backend(listaValores)
+        
+
     if resposta != True:
         tk.messagebox.showinfo("Erro", "Não foi possivel cadastrar, verifique os valores digitados")
-    else:    
+    else:
+        # adicionei essa linha abaixo para limpar a saida toda vez que for uma noa requisição
+        text_widget.delete("1.0","end")    
+        text_widget.insert("end",f"Úlltimo cadastro: {row}\n")    
         tk.messagebox.showinfo("Sucesso", "Dados inseridos com sucesso!")
         
     return resposta
@@ -36,16 +48,14 @@ def inserir_dados():
 def atualizar_dados():
     pass
 
-def deletar_dados():
+def deletar_dados_pelo_cpf():
     pass
 
-def fechar_app():
-    pass
 
 # criação da janela principal
 janela = tk.Tk()
 janela.title("Cadastro Clientes")
-janela.geometry("290x300")
+janela.geometry("585x310")
 width_entry = 30
 
 # legenda campo de entrada
@@ -62,31 +72,35 @@ email = tk.Entry(janela)
 cpf = tk.Entry(janela)
 
 # criando botoes
-capturar_input = tk.Button(janela, text="Salvar dados", command=obter_dados_input)
 ver_todos_registros = tk.Button(janela, text="Ver todos registros", command=buscar_todos_dados)
-ver_busca = tk.Button(janela, text="Buscar", command=buscar)
+ver_busca = tk.Button(janela, text="Buscar pelo cpf", command=buscar_pelo_cpf)
+ver_dados_deletados = tk.Button(janela, text="Deletar pelo cpf", command=deletar_dados_pelo_cpf)
+ver_dados_atualizados = tk.Button(janela, text="Atualizar contatos", command=atualizar_dados)
 ver_dados_inseridos = tk.Button(janela, text="Inserir", command=inserir_dados)
-ver_dados_atualizados = tk.Button(janela, text="Atualizar", command=atualizar_dados)
-ver_dados_deletados = tk.Button(janela, text="Deletar", command=deletar_dados)
-fechar_aplicativo = tk.Button(janela, text="Fechar", command=fechar_app)
+
+
+# criando espaço texto vazio para inserir informações e um com scroolbar lateral "widget"
+text_widget = tk.Text(janela, wrap="word", height=5)
+scrollbar = tk.Scrollbar(janela, command=text_widget.yview)
+text_widget.config(yscrollcommand=scrollbar.set)
+
 
 
 # orientacao dos campos na janela
-label_nome.grid(row=0, column=0, sticky="w")
-nome.grid(row=0, column=1)
-label_sobrenome.grid(row=1, column=0, sticky="w")
-sobrenome.grid(row=1, column=1)
-label_email.grid(row=2, column=0, sticky="w")
-email.grid(row=2, column=1)
-label_cpf.grid(row=3, column=0, sticky="w")
-cpf.grid(row=3, column=1)
-ver_todos_registros.grid(row=5, column=0, columnspan=3)
-ver_busca.grid(row=6, column=0, columnspan=3)
-ver_dados_inseridos.grid(row=7, column=0, columnspan=3)
-ver_dados_atualizados.grid(row=8, column=0, columnspan=3)
-ver_dados_deletados.grid(row=8, column=0, columnspan=3)
-capturar_input.grid(row=9, column=0, columnspan=3)
-fechar_aplicativo.grid(row=10, column=0, columnspan=3)
-
+label_nome.grid(row=0, column=0, sticky="w", columnspan=2)
+nome.grid(row=0, column=1, columnspan=2)
+label_sobrenome.grid(row=1, column=0, sticky="w", columnspan=2)
+sobrenome.grid(row=1, column=1, columnspan=2)
+label_email.grid(row=2, column=0, sticky="w", columnspan=2)
+email.grid(row=2, column=1, columnspan=2)
+label_cpf.grid(row=3, column=0, sticky="w", columnspan=2)
+cpf.grid(row=3, column=1, columnspan=2)
+ver_todos_registros.grid(row=4, column=0, columnspan=3)
+ver_busca.grid(row=5, column=0, columnspan=3)
+ver_dados_deletados.grid(row=6, column=0, columnspan=3)
+ver_dados_atualizados.grid(row=7, column=0, columnspan=3)
+ver_dados_inseridos.grid(row=8, column=0, columnspan=3)
+text_widget.grid(row=9, column=0, sticky="w", columnspan=2)
+scrollbar.grid(row=9, column=2, sticky="w", columnspan=2)
 
 janela.mainloop()
